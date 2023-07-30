18b933410cbeb8c4864a5f251e6df5d9a70bb7da
isuranga <deshikaisu95@gmail.com>
2023-07-30 23:27:18 +0530

    Complete Task 5

---
+++ DemoTask5.java
@@ -0,0 +1,17 @@
+public class DemoTask5 {
+    public static void main(String[] args) {
+        final String num1 = "\033[30;1m";
+        
+        final String num2 = "\033[31;1m";
+        
+        final String num3 = "\033[32;1m";
+        
+        final String num4 = "\033[33;1m";
+        
+        
+         final String Reset = "\033[35;0m";
+
+        
+        System.out.printf("%s978%s-%s3%s-%s16%s-%s1484100%s \n" ,num1,Reset,num2,Reset,num3,Reset,num4, Reset);
+    }
+}
